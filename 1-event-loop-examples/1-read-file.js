9ae7501582245258b48aa982e505eee3c8614171
developer1607 <developer1607@gmail.com>
2025-11-18 16:15:12 +0530

    latest commit 2:38:27

--- 1-event-loop-examples/1-read-file.js
+++ 1-event-loop-examples/1-read-file.js
@@ -1,13 +1,13 @@
-const { readFile } = require('fs')
+const { readFile, writeFile } = require('fs')
 
-console.log('started the first task ')
-// Check the file path
-readFile('../content/first.txt', 'utf8', (err, result)=>{
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log(result)
-    console.log('completed the first task ')
+console.log('started a first task')
+// CHECK FILE PATH!!!!
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log(result)
+  console.log('completed first task')
 })
-console.log('starting the next task')
+console.log('starting next task')
